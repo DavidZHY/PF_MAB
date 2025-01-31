8c1a7ca1dde68ce33ecd1c2cb407275c27e9bb9d
RattleSnack <david.z2016@outlook.com>
2025-01-31 11:18:23 +0800

    删除了不必要的空格和换行

--- client.py
+++ client.py
@@ -17,7 +17,7 @@ class client(object):
         self.M = nclients
         self.alpha = palpha
         self.fp = fp
-        
+
         self.p = 1
         self.local_set = set(np.arange(self.K))
         self.global_set = set(np.arange(self.K))
@@ -25,43 +25,43 @@ class client(object):
         self.global_mean = np.zeros(self.K)
         self.mixed_mean = np.zeros(self.K)
         self.reward = np.zeros(self.K)
-        
+
         self.pull = np.zeros(self.K)
         self.p_length = self.fp(self.p)
         self.Fp = 0
-        
+
         self.fphase = 0
         self.gphase = 0
-        
+
         self.F = -1
         self.l_exploration = False
         self.g_exploration = False
-    
+
     def play(self):
         if self.fphase < np.ceil((1-self.alpha)*self.p_length)*len(self.global_set): #global exploration
 #            print("loc", self.p)
             #play = list(self.global_set)[self.fphase%len(self.global_set)]
             play = list(self.global_set)[int(self.fphase//(np.ceil((1-self.alpha)*self.p_length)))]
             self.fphase += 1
-            
+
         elif self.gphase < np.ceil(self.M*self.alpha*self.p_length)*len(self.local_set): #local exploration
 #            print("glob", self.p)
             #play = list(self.local_set)[self.gphase%len(self.local_set)]
             play = list(self.local_set)[int(self.gphase//(np.ceil(self.M*self.alpha*self.p_length)))]
             self.gphase += 1
-            
+
         else: #exploitation phase
             if self.l_exploration is True:
                 play = self.F
             else:
                 play = np.argmax(self.alpha*self.local_mean+(1-self.alpha)*self.global_mean)
-            
+
         return play
-    
+
     def reward_update(self,play,obs):
         self.reward[play] += obs
         self.pull[play] += 1
-        
+
     def local_mean_update(self):
         #print('global',self.fphase,np.ceil((1-self.alpha)*self.p_length)*len(self.global_set))
         #print('local',self.gphase,np.ceil(self.M*self.alpha*self.p_length)*len(self.local_set))
@@ -71,11 +71,11 @@ class client(object):
             return True, self.local_mean
         else:
             return False, 0
-        
+
     def global_mean_update(self,global_stat):
         self.global_mean = global_stat
         self.mixed_mean = self.alpha*self.local_mean+(1-self.alpha)*self.global_mean
-        
+
     def local_set_update(self):
         Ep = set()
         self.Fp += self.p_length
@@ -84,16 +84,15 @@ class client(object):
             if self.mixed_mean[i]+conf_bound < max(self.mixed_mean-conf_bound):
                 Ep.add(i)
         self.local_set = self.local_set-Ep
-        
+
         if len(self.local_set) == 1 and self.l_exploration is False:
             self.l_exploration = True
             self.F = list(self.local_set)[0]
             #print("player", self.id,  " fixate",self.F)
             self.local_set = set()
-        #print("player", self.id, " local-set:",self.local_set)    
+        #print("player", self.id, " local-set:",self.local_set)
         return self.local_set
-        
-        
+
     def global_set_update(self,global_set):
         self.global_set = global_set
         self.p +=1
@@ -102,8 +101,3 @@ class client(object):
         self.fphase = 0
         self.gphase = 0
         self.g_exploration = (len(self.global_set)==0)
-        
-            
-            
-    
-            
--- server_improved.py
+++ server_improved.py
@@ -8,19 +8,19 @@ class imp_server(object):
                  nclients):
         self.M = nclients
         self.K = narms
-        
+
         self.local_means = np.zeros([self.M,self.K])
         self.global_means = np.zeros(self.K)
         self.global_set = set()
         self.global_delta = np.ones([self.M, self.K])
-        
+
         self.p = 1
         self.c_local_stat = np.zeros(self.M)
-        
+
     def local_mean_update(self,i,local_stat):
         self.local_means[i] = local_stat
         self.c_local_stat[i] = 1
-        
+
     def global_mean_update(self):
         self.global_set = set()
         if sum(self.c_local_stat) >= self.M:
@@ -29,16 +29,16 @@ class imp_server(object):
             return True, self.global_means
         else:
             return False, 0
-       
+
     def local_set_update(self,i,local_set):
         self.global_set = self.global_set|local_set
-    
+
     def local_delta_update(self,i,local_delta):
         self.global_delta[i] = local_delta
-    
+
     def global_set_update(self):
         return self.global_set
-    
+
     def global_delta_update(self):
         #print("global delta:", self.global_delta)
         return self.global_delta
